f625aac1537bb8a6f712457d57a20043dff7a069
Paulina594 <paulina.drozdz94@gmail.com>
2022-07-19 17:59:07 +0200

    [PB-9] genre list created

--- src/app/pages/genres-page/genres-page.component.ts
+++ src/app/pages/genres-page/genres-page.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { GenreService } from '../../services/genre.service';
+import { Genre } from '../../models/Genre';
 
 @Component({
   selector: 'app-genres-page',
   templateUrl: './genres-page.component.html',
-  styleUrls: ['./genres-page.component.scss']
+  styleUrls: ['./genres-page.component.scss'],
 })
 export class GenresPageComponent implements OnInit {
+  genres: Genre[];
 
-  constructor() { }
+  constructor(private genreService: GenreService) {}
 
   ngOnInit(): void {
+    this.genreService.getGenres().subscribe((genres) => (this.genres = genres));
   }
-
 }
